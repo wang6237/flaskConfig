10c5c43f4522a10d52268d2d2b0dddae883fb1e8
wang6237 <ykwang@vip.qq.com>
2020-04-12 23:08:26 +0800

    删除多余代码

--- init_db.py
+++ init_db.py
@@ -13,21 +13,12 @@
 """
 __author__ = 'wang6237'
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# from models.EtcdModel import Etcd
-# from models.ProjectModel import Project
-# from models.ContrlModel import Control
-# from models.UserModel import User
-# from models import db, reset_database
+
 from app import db, User
 from app import Generate_pwd
 
 db.create_all()
 db.session.add(User(username='admin', password=Generate_pwd('111111'), email='ykwang@vip.qq.com', role='admin'))
-# db.session.add(Project(''))
-# db.session.commit()
-# print(User.query.filter_by(username='test').update({'role': 'dev'}))
-# db.session.add(User)
+
 db.session.commit()
 
